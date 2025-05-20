c73f4142bd33ca1ea70153e27cfc75ab87eba712
trist <trist@LAPTOP-SG1LNF3R>
2025-05-20 17:52:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG172_182Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
